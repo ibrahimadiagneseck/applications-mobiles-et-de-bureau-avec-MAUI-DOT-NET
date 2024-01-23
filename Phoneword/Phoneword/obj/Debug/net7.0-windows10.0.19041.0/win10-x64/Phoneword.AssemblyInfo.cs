b17452785dc14cf26a12f193b1691f66498ee0f1
Ibrahima Diagne Seck <ibrahimadiagneseck@esp.sn>
2024-01-23 16:46:15 +0000

    5-exercise-create-first-xaml-page

--- Phoneword/Phoneword/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/Phoneword.AssemblyInfo.cs
+++ Phoneword/Phoneword/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/Phoneword.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Phoneword")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0a27689acd94a7789004c894bde7a3092c71a01d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Phoneword")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Phoneword")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
