85a4b3e504a101a13cf9f2797b01489d6b5ab2c8
Ibrahima Diagne Seck <ibrahimadiagneseck@esp.sn>
2024-01-30 02:01:45 +0000

    6-xaml-markup-extensions

--- mslearn-dotnetmaui-create-user-interface-xaml/exercise1/Notes/MainPage.xaml.cs
+++ mslearn-dotnetmaui-create-user-interface-xaml/exercise1/Notes/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Notes;
+﻿using Microsoft.Maui.Controls;
+
+namespace Notes;
 
 /*public partial class MainPage : ContentPage
 {
@@ -55,6 +57,7 @@
 public partial class MainPage : ContentPage
 {
     string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    /*Editor editor;*/
 
     public MainPage()
     {
@@ -70,4 +73,14 @@ public partial class MainPage : ContentPage
     {
         File.WriteAllText(_fileName, editor.Text);
     }
+
+    void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+        }
+        editor.Text = string.Empty;
+    }
 }
+
