5dd79dedb330fe7f2dab7dd2645e900484c07d51
Ibrahima Diagne Seck <ibrahimadiagneseck@esp.sn>
2024-02-07 13:56:34 +0000

    7-exercise-use-tabbed-pages-with-navigation-pages

--- mslearn-dotnetmaui-create-multi-page-apps/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
+++ mslearn-dotnetmaui-create-multi-page-apps/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
@@ -1,10 +1,30 @@
 namespace Astronomy.Pages;
 
+[QueryProperty(nameof(AstroName), "astroName")]
 public partial class AstronomicalBodiesPage : ContentPage
 {
+    string astroName;
+    public string AstroName
+    {
+        get => astroName;
+        set
+        {
+            astroName = value;
+
+            // this is a custom function to update the UI immediately
+            // UpdateAstroBodyUI(astroName);
+        } 
+    }
 	public AstronomicalBodiesPage()
 	{
 		InitializeComponent();
 
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=comet");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=earth");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=moon");
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=sun");
+
     }
+
+
 }
